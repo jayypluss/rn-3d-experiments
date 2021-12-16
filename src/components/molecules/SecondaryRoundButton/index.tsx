9b8a5cde6cf72a06e818ac53447cc5aa93318c2a
Jonathan Ferreira Kuntz <jonathanfk1@gmail.com>
2021-12-16 18:52:41 -0300

    Creating interpolating border color animation for Secondary Round Button.

--- src/components/molecules/SecondaryRoundButton/index.tsx
+++ src/components/molecules/SecondaryRoundButton/index.tsx
@@ -1,6 +1,6 @@
-import React, {FunctionComponent} from 'react'
+import React, {FunctionComponent, useState} from 'react'
 import {TouchableOpacity} from 'react-native-gesture-handler'
-import {Text} from 'react-native'
+import {Animated, Text} from 'react-native'
 
 interface ButtonProps {
   title: string
@@ -11,12 +11,35 @@ export const SecondaryRoundButton: FunctionComponent<ButtonProps> = ({
   title,
   action,
 }) => {
+  const [animation, setAnimation] = useState(new Animated.Value(0))
+
+  let springAnimation = Animated.loop(
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: false,
+    }),
+  )
+
+  // Animated.timing(animation, {
+  //   toValue: 1,
+  //   duration: 1000,
+  //   useNativeDriver: false,
+  // }).start()
+
+  springAnimation.start()
+
+  const boxInterpolation = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: ['rgb(100,255,100)', 'rgb(100, 0, 100)', 'rgb(100,255,100)'],
+  })
+
   return (
     <TouchableOpacity
       style={{
         height: 65,
         width: 185,
-        borderColor: 'rgba(0, 0, 0, 0.9)',
+        borderColor: boxInterpolation,
         borderWidth: 4,
         alignItems: 'center',
         justifyContent: 'center',
